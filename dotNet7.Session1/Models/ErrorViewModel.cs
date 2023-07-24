9fba0ab177e3ed0c33a75610ba0aa951140ac9c5
Cairo <oriac.world2017@gmail.com>
2023-07-24 13:36:03 +0200

    Add project files.

---
+++ dotNet7.Session1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dotNet7.Session1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
